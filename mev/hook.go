1270830bf5324c18c421082d5eec28727206f772
Ha Thanh Tam <thanhtam.ha1994@hotmail.com>
2026-01-16 11:45:25 +0700

    Fix

--- mev/hook.go
+++ mev/hook.go
@@ -1,64 +1,38 @@
 package mev
 
 import (
-	"math/big"
-	"sync"
+	"encoding/hex"
+	"fmt"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
 )
 
-var (
-	signerMu    sync.RWMutex
-	signerCache = map[string]types.Signer{}
-)
-
-func cachedSigner(chainID *big.Int) types.Signer {
-	key := chainID.String()
-
-	signerMu.RLock()
-	s, ok := signerCache[key]
-	signerMu.RUnlock()
-	if ok {
-		return s
+func OnRawTxFromPeer(
+	tx *types.Transaction,
+	peerID string,
+	ts time.Time,
+) {
+	if tx == nil {
+		return
 	}
 
-	s = types.LatestSignerForChainID(chainID)
-
-	signerMu.Lock()
-	signerCache[key] = s
-	signerMu.Unlock()
-
-	return s
-}
-
-func OnRawTxFromPeer(tx *types.Transaction, peer string, ts time.Time) {
 	if !PassFilter(tx) {
 		return
 	}
 
-	from, err := types.Sender(cachedSigner(tx.ChainId()), tx)
+	hash := tx.Hash()
+
+	raw, err := tx.MarshalBinary()
 	if err != nil {
 		return
 	}
 
-	raw, err := rlp.EncodeToBytes(tx)
-	if err != nil {
-		return
-	}
-
-	ev := &TxEvent{
-		Hash:   tx.Hash().Hex(),
-		From:   from.Hex(),
-		To:     tx.To().Hex(),
-		Value:  tx.Value().String(),
-		Input:  hexutil.Encode(tx.Data()),
-		RawTx:  hexutil.Encode(raw),
-		Peer:   peer,
-		TsNano: ts.UnixNano(),
-	}
-
-	send(ev)
+	fmt.Printf(
+		"[MEV] %s hash=%s rawTx=0x%s peer=%s\n",
+		ts.Format(time.RFC3339Nano),
+		hash.Hex(),
+		hex.EncodeToString(raw),
+		peerID,
+	)
 }
