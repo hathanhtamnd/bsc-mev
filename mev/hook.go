bf2c5cc24e8165dedb4482ea69056497a1425419
Ha Thanh Tam <thanhtam.ha1994@hotmail.com>
2026-01-16 11:51:07 +0700

    Fix

--- mev/hook.go
+++ mev/hook.go
@@ -3,10 +3,53 @@ package mev
 import (
 	"encoding/hex"
 	"fmt"
+	"net"
+	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/core/types"
 )
+
+var (
+	conn     net.Conn
+	connOnce sync.Once
+	connMu   sync.Mutex
+)
+
+func initTCP() {
+	connOnce.Do(func() {
+		go func() {
+			for {
+				c, err := net.Dial("tcp", "127.0.0.1:8999")
+				if err != nil {
+					fmt.Println("[MEV] dial 8999 failed, retry in 1s:", err)
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				connMu.Lock()
+				conn = c
+				connMu.Unlock()
+				fmt.Println("[MEV] connected to 127.0.0.1:8999")
+				return
+			}
+		}()
+	})
+}
+
+func writeLine(s string) {
+	connMu.Lock()
+	defer connMu.Unlock()
+	if conn == nil {
+		return
+	}
+	_, err := conn.Write([]byte(s))
+	if err != nil {
+		fmt.Println("[MEV] write failed:", err)
+		_ = conn.Close()
+		conn = nil
+		connOnce = sync.Once{}
+	}
+}
 
 func OnRawTxFromPeer(
 	tx *types.Transaction,
@@ -16,6 +59,8 @@ func OnRawTxFromPeer(
 	if tx == nil {
 		return
 	}
+
+	initTCP()
 
 	if !PassFilter(tx) {
 		return
@@ -28,11 +73,13 @@ func OnRawTxFromPeer(
 		return
 	}
 
-	fmt.Printf(
-		"[MEV] %s hash=%s rawTx=0x%s peer=%s\n",
+	line := fmt.Sprintf(
+		"%s hash=%s rawTx=0x%s peer=%s\n",
 		ts.Format(time.RFC3339Nano),
 		hash.Hex(),
 		hex.EncodeToString(raw),
 		peerID,
 	)
+
+	writeLine(line)
 }
