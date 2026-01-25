a41ac781b607aa84412aad8d7c0d5607be9900b1
Ha Thanh Tam <thanhtam.ha1994@hotmail.com>
2026-01-26 02:08:13 +0700

    Add Filter

--- mev/filter_pancake_v2.go
+++ mev/filter_pancake_v2.go
@@ -11,6 +11,7 @@ import (
 var (
 	allowedToPancakeV2 = map[common.Address]struct{}{
 		common.HexToAddress("0x10ED43C718714eb63d5aA57B78B54704E256024E"): {},
+		common.HexToAddress("0x13f4EA83D0bd40E75C8222255bc855a974568Dd4"): {},
 	}
 
 	seenTxsPancakeV2 sync.Map
