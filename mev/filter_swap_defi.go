d9aefc8a92221f7609f096444f33ef794241f157
Ha Thanh Tam <thanhtam.ha1994@hotmail.com>
2026-01-26 10:00:04 +0700

    Update

--- mev/filter_swap_defi.go
+++ mev/filter_swap_defi.go
@@ -12,7 +12,7 @@ var (
 	allowedTo = map[common.Address]struct{}{
 		common.HexToAddress("0x10ED43C718714eb63d5aA57B78B54704E256024E"): {},
 		common.HexToAddress("0x13f4EA83D0bd40E75C8222255bc855a974568Dd4"): {},
-		common.HexToAddress("0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B"): {},
+		common.HexToAddress("0xd9C500DfF816a1Da21A48A732d3498Bf09dc9AEB"): {},
 	}
 
 	seenTxs sync.Map
